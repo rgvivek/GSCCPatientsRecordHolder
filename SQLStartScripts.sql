86b0ecb11092635578aab263988fc773a250a112
rgvivek <rgvivek@gmail.com>
2017-07-02 12:50:16 +1000

    Updating table scripts

--- SQLStartScripts.sql
+++ SQLStartScripts.sql
@@ -35,4 +35,26 @@ CREATE TABLE `users` (
 
 
 
+
+CREATE TABLE `patienthistory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `patientid` int(11) DEFAULT NULL,
+  `siblingscount` int(11) DEFAULT NULL,
+  `ageofmarriage` int(11) DEFAULT NULL,
+  `kidscount` int(11) DEFAULT NULL,
+  `maritalstatus` varchar(45) DEFAULT NULL,
+  `smoking` varchar(45) DEFAULT NULL,
+  `alcohol` varchar(45) DEFAULT NULL,
+  `chewing` varchar(45) DEFAULT NULL,
+  `eatinghabit` varchar(45) DEFAULT NULL,
+  `isdiabetic` tinyint(1) DEFAULT NULL,
+  `ishypertensive` tinyint(1) DEFAULT NULL,
+  `otherillness` text,
+  `pastmedicalhistory` text,
+  `currentmedications` text,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (patientid) REFERENCES patients(id)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
 INSERT INTO gscc.users (firstname,lastname,username,password,isAdmin) values ('GSCC','Admin','admin', '$2a$08$Vt3fKqJHuo0QfIXrNvKV6e/uyFC1CBOf4Qmefvn7crFWKaR9D0vNK', true);
